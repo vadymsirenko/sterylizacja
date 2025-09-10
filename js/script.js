c359cd934d61cfdfdb21d664c8f22690c7674cd1
VS hp <magnipython@gmail.com>
2025-09-11 01:37:34 +0200

    add swiper

--- js/script.js
+++ js/script.js
@@ -112,3 +112,32 @@ document.getElementById("y").textContent = new Date().getFullYear();
       closePopup();
     }
   });
+
+  // Swiper
+  const swiper = new Swiper('.swiper', {
+  // Optional parameters
+ spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        reverseDirection: false,
+      },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
